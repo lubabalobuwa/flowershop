e5b4f477ad05ff69b2820a86ccd4e8a57356c148
Lubabalo Buwa <G17B3155@ict.ru.ac.za>
2019-06-01 15:25:51 +0200

    Added a test method

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using NSubstitute;
+using FlowerShop;
 
 namespace Tests
 {
@@ -12,7 +14,14 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //Arrange
+            IOrderDAO x = Substitute.For<IOrderDAO>(); // mock for IOrderDAO
+            IClient y = Substitute.For<IClient>(); // MOCK FOR IClient 
+            IOrder p = new Order(x, y);
+            //Act
+            p.Deliver();
+            //Assert
+            x.Received().SetDelivered(p);
         }
     }
 }
